9e26cf8f79716c3a775202314c88bfad6028b230
Zak Maaz <zaktwo@users.noreply.github.com>
2023-06-26 16:31:37 +0100

    update

--- script.js
+++ script.js
@@ -4,7 +4,19 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 // Starting Conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+//Rolling dice functionallity
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // 2. diplay dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = 'dice-${dice}.png';
+  //3. Check for rolled 1: if true; switch to next player
+});
