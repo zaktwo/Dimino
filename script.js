c185f5753adeb5f3ab2288b3998be904c96465b4
Zak Maaz <zaktwo@users.noreply.github.com>
2023-06-27 15:28:14 +0100

    update

--- script.js
+++ script.js
@@ -13,7 +13,24 @@ const player1 = document.querySelector('.player--0');
 const player2 = document.querySelector('.player--1');
 const active = document.querySelector('.player--active');
 const currentPlayer = document.querySelector('.player');
-//variables
+//variables 
+// you can create an init function that have all the variables initilazed in it
+/*
+let scores, currentScore, activePlayer, playing;
+const init = function(){
+scores = [0, 0];
+currentScore = 0;
+activePlayer = 0;
+playing = true;
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+}
+declare it outside 
+init();
+and use it inside the new game button
+*/
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
@@ -62,7 +79,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // check if player's score is >=100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       document
         .querySelector(`.player--${activePlayer}`)
@@ -70,9 +87,30 @@ btnHold.addEventListener('click', function () {
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.remove('player--active');
-        diceEl.classList.add('hidden');
+      diceEl.classList.add('hidden');
     } else {
       switchPlayer();
     }
   }
+});
+
+btnNew.addEventListener('click', function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  currentScorre0.textContent = 0;
+  currentScorre1.textContent = 0;
+  if (
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.contains('player--winner')
+  ) {
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--winner');
+  }
+  player1.classList.add('player--active');
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 });
